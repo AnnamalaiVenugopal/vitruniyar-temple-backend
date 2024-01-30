91d3c2cca817dca917043c362c033f8f5a7c6f6a
Annamalai <hasanmalai96@gmail.com>
2024-01-30 11:40:55 +0530

    Committed for changing the ID generator schema.

--- src/main/java/com/vitruniyar/temple/entity/ArchanaTicket.java
+++ src/main/java/com/vitruniyar/temple/entity/ArchanaTicket.java
@@ -1,14 +1,9 @@
 package com.vitruniyar.temple.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
-import org.hibernate.id.enhanced.SequenceStyleGenerator;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -20,16 +15,8 @@ import java.time.LocalTime;
 @Entity
 public class ArchanaTicket {
     @Id
-    @GeneratedValue
-    @GenericGenerator(
-            name = "sequence-generator",
-            type = SequenceStyleGenerator.class,
-            parameters = {
-                    @Parameter(name = "sequence_name", value = "VIT-ARCH-"),
-                    @Parameter(name = "initial_value", value = "1000"),
-                    @Parameter(name = "increment_size", value = "10")
-            }
-    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "archana_ticket_seq")
+    @SequenceGenerator(name = "archana_ticket_seq", sequenceName = "archana_ticket_seq", initialValue = 1000, allocationSize = 1)
     private long ticketId;
     private String name;
     private String phoneNumber;
